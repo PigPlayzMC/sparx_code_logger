1bd79928c5bcb96bb857c8d2f22265476f89a7d4
PigPlayzMC <charliehbird@gmail.com>
2025-05-05 13:37:03 +0100

    Added colour to console log messages to enhance debugging readability

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 //! This only runs on Sparx Maths. Do not put extension code here!
 
-console.log("SBCL: Active");
+console.log("%cSBCL: Active", 'color:rgb(247, 255, 129)');
 
 const script = document.createElement('script');
 script.src = chrome.runtime.getURL('injected.js'); // External file in extension
@@ -9,7 +9,7 @@ script.onload = () => script.remove();
 
 // Listen for events from page context
 window.addEventListener('console-log-intercepted', (e) => {
-    console.log('SBCL Interceptor:', ...e.detail); // Space char at front?
+    console.log('SBCL Interceptor:', ...e.detail); // Cannot be coloured
     
     const message = e.detail;
     ////console.log(message);
@@ -27,13 +27,13 @@ window.addEventListener('console-log-intercepted', (e) => {
     // [ACT] START fd36758f-4f25-4b05-953b-8e66030f8240 2 7 undefined
     // [ACT] START fd36758f-4f25-4b05-953b-8e66030f8240 2 8 undefined
     if (Array.isArray(message) && message[0] == ("[ACT] START")) {
-        console.log("SBCL: New question started");
+        console.log("%cSBCL: New question started", 'color:rgb(247, 255, 129)');
         ////console.log("SBCL Message: " + message);
 
         const task = message[2];
         const question = message[3]; // Int rather than char
 
-        console.log("SBCL Task: " + task);
-        console.log("SBCL Question: " + question);
+        console.log("%cSBCL Task: " + task, 'color:rgb(247, 255, 129)');
+        console.log("%cSBCL Question: " + question, 'color:rgb(247, 255, 129)');
     }
 });
